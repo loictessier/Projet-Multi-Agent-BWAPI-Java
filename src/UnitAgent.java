a897f0215ad4018936c272a92aa9c05224c3a44c
Habaieb Abderahmane <argento.bu@gmail.com>
2015-04-20 19:59:58 -0400

    Ajout de la machine à états Ajout du message dispatcher non fini

--- src/UnitAgent.java
+++ src/UnitAgent.java
@@ -1,34 +1,28 @@
 
-public abstract class UnitAgent {
-	//every entity must have a unique identifying number
-	private int m_ID;
-		 
-	//this is the next valid ID. Each time a BaseGameEntity is instantiated
-	//this value is updated
-	private static int  m_iNextValidID;
-		 
+public class UnitAgent extends Agent {
+	//an instance of the state machine class
+	private StateMachine<UnitAgent>  m_pStateMachine;
+	
 	public UnitAgent(int id) {
-		SetID(id);
+		super(id);
+		
+		//set up state machine
+	    m_pStateMachine = new StateMachine<UnitAgent>(this);
+	     
+	    //m_pStateMachine.setCurrentState(GoHomeAndSleepTilRested::Instance());
 	}
-	
-	//all entities must implement an update function
-	public abstract void Update();
-		 
-	//all entities can communicate using messages. They are sent
-	//using the MessageDispatcher singleton class
-	public abstract boolean HandleMessage(Message msg);
-	
-	public int ID() {
-		return m_ID;
+
+	@Override
+	public void Update() {	   
+		m_pStateMachine.Update();
 	}
-	
-	//this must be called within the constructor to make sure the ID is set
-	//correctly. It verifies that the value passed to the method is greater
-	//or equal to the next valid ID, before setting the ID and incrementing
-	//the next valid ID
-	private void SetID(int val) {
-		assert val >= m_iNextValidID : "<BaseGameEntity::SetID>: invalid ID";
-		m_ID = val;
-	    m_iNextValidID = m_ID + 1;
+
+	@Override
+	public boolean HandleMessage(Message msg) {
+		return m_pStateMachine.HandleMessage(msg);
+	}
+
+	public StateMachine<UnitAgent> GetFSM() {
+		return m_pStateMachine;
 	}
 }
